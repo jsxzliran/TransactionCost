ae94b0684563d79dd58c0c4a658e5ca41a7f055c
jsxzliran <82885829+jsxzliran@users.noreply.github.com>
2021-04-27 17:40:25 +0100

    Add files via upload

---
+++ Data_generator.py
@@ -0,0 +1,39 @@
+import numpy as np
+from datetime import datetime
+import pandas as pd
+import math
+import bottleneck as bn
+
+# a class describing the features of a simulated path matrix
+class OneStock:
+    def __init__(self, seed, mu, sigma, s0, npaths, nsteps, T):
+        self.seed = seed
+        self.mu = mu
+        self.sigma = sigma
+        self.s0 = s0
+        self.npaths = npaths
+        self.nsteps = nsteps
+        self.T = T
+    
+    # create a price matrix
+    def Prices(self):
+        np.random.seed(self.seed)
+        dt = self.T/self.nsteps
+        W1 = np.random.normal(size=(self.nsteps,self.npaths))
+        logS0 = np.log(self.s0)
+        dlogS = (self.mu - 1/2*self.sigma*self.sigma)*dt+math.sqrt(dt)*self.sigma*W1
+        prices = np.exp(logS0+np.cumsum(dlogS,axis=0))
+        prices = np.transpose(np.row_stack((self.s0*np.ones((1,self.npaths)),prices)))
+        return prices
+
+    # create a return matrix
+    def Returns(self):
+        np.random.seed(self.seed)
+        dt = self.T/self.nsteps
+        W2 = np.random.normal(size=(self.nsteps,self.npaths))
+        logS0 = np.log(self.s0)
+        dlogS = (self.mu - 1/2*self.sigma*self.sigma)*dt+math.sqrt(dt)*self.sigma*W2
+        prices = np.exp(logS0+np.cumsum(dlogS,axis=0))
+        prices = np.row_stack((self.s0*np.ones((1,self.npaths)),prices))
+        returns = np.transpose(np.exp(np.diff(np.log(prices), axis=0))-1)
+        return returns
---
+++ UtilityLoss.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import bottleneck as bn
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from torch.autograd import Variable
+from torch import optim
+import math, random
+
+# Define 2 utility function, log utility and power utility
+class LogUtilityLoss(torch.nn.Module):
+    
+    def __init__(self):
+        super(LogUtilityLoss,self).__init__()
+        
+    def forward(self,x):
+        loss = -torch.mean(torch.log(x))
+        return loss
+
+class PowerUtilityLoss(torch.nn.Module):
+    
+    def __init__(self,gamma):
+        super(PowerUtilityLoss,self).__init__()
+        self.gamma = gamma
+        
+    def forward(self,x):
+        loss = -torch.mean((torch.pow(x,1-self.gamma)-1)/(1-self.gamma))
+        return loss
